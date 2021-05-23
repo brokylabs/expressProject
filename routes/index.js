d189213ac67a2d70a43da82e9ef5be26c3202193
brokylabs <c.meong@gmail.com>
2021-05-23 10:26:05 +0700

    setup routes index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+const { route } = require('./users');
 var router = express.Router();
 
-/* GET home page. */
+// Welcome Page
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('welcome', { title: 'Welcome Page' });
 });
+
+// Dashboard Page
+router.get('/dashboard', function(req, res, next){
+  res.render('dashboard', { title : 'Dashboard Page' })
+})
 
 module.exports = router;
